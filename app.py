43824c067a900b9c6a9e41b440359c13c682f1a0
Titas <titas.januson@gmail.com>
2021-12-02 16:13:35 +0000

    Add simple user GUI with dynamic issue cards

--- app.py
+++ app.py
@@ -1,11 +1,42 @@
+import json
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+# Token for map API
+token = "pk.eyJ1Ijoic3lyb3F0IiwiYSI6ImNrd2d1M2dwOTBzMHoyd21vaXUwemZsZHYifQ.qMKLv7M4w6lRtfaDopK73A"
 
 
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+
+@app.route("/user")
+def user():
+    # dummy data which should be retrieved from DB
+    issues = [
+        {  # Helix Sq
+            "location": [-1.6268, 54.9729],
+            "color": "black",
+            "id": 363,
+            "description": "This is a description",
+            "category": "This is the category",
+            "upvotes": 500,
+            "downvotes": 60,
+        },
+        {  # USB
+            "location": [-1.62494, 54.9735751],
+            "color": "blue",
+            "id": 696,
+            "description": "This is a description",
+            "category": "This is the category",
+            "upvotes": 555,
+            "downvotes": 600,
+        },
+    ]
+    return render_template("user.html", token=token, issues=issues)
 
 
 if __name__ == "__main__":
---
+++ static/src/user.js
@@ -0,0 +1,6 @@
+// Front end functions for user GUI and functionality
+function init(){
+    console.log("Init funct");
+    const menu = document.getElementById("menu");
+    menu.addEventListener("click", e => {console.log("menu click");})
+}
