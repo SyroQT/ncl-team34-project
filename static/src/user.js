3dd7fe6830ccce99f9c598cf2dee77399ef63f3b
Titas <titas.januson@gmail.com>
2021-12-03 15:35:58 +0000

    Add empty menu functionality

--- static/src/user.js
+++ static/src/user.js
@@ -1,6 +1,1 @@
 // Front end functions for user GUI and functionality
-function init(){
-    console.log("Init funct");
-    const menu = document.getElementById("menu");
-    menu.addEventListener("click", e => {console.log("menu click");})
-}
