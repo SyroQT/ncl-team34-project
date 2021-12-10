52adf4e515bde7c2df973d644572148759206065
AreeshImran <c0040696@newcastle.ac.uk>
2021-12-10 16:31:52 +0000

    Updated RegisterForm which will be used to implement backend functionality

--- users/forms.py
+++ users/forms.py
@@ -3,10 +3,19 @@ from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import Required, Email, ValidationError, Length, EqualTo
 
 
+# checks if a field contains characters that are not allowed
+def character_check(form, field):
+    excluded_chars = "*?!'^+%&/()=}][{$#@<>"
+    for char in field.data:
+        if char in excluded_chars:
+            raise ValidationError(
+                f"Character {char} is not allowed.")
+
+
 class RegisterForm(FlaskForm):
     username = StringField(validators=[Required()])
-    firstname = StringField(validators=[Required()])
-    lastname = StringField(validators=[Required()])
+    firstname = StringField(validators=[Required(), character_check])
+    lastname = StringField(validators=[Required(), character_check])
     email = StringField(validators=[Required(), Email()])
     phone = StringField(validators=[Required()])
     password = PasswordField(validators=[Required(), Length(min=6, max=12,
